154ec97da825555c1aa85cf1a843a1a08ac17068
JEETDESAI25 <jeetdesai25@gmail.com>
2023-03-06 19:34:58 -0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./contactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Dipesh",
+      email: "nicks@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Nipesh",
+      email: "malvia@gmail.com",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
